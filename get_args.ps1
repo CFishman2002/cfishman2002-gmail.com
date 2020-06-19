db9ea00e57856ea359c6a60d1dcff1b7f9a93352
CFishman2002 <63076924+CFishman2002@users.noreply.github.com>
2020-06-19 14:06:08 -0400

    Update get_args.ps1

--- get_args.ps1
+++ get_args.ps1
@@ -2,4 +2,4 @@ $userobjects = Import-CSV Args.txt -Header idx,name,value
 
 $var_num = $userobjects[0].name
 Copy-Item Args.txt -Destination Args2.txt
-Write-Host $var_num
+Write-Host "CLF $var_num"
