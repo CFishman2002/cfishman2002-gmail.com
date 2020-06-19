33f3fe532968e96ee7c0945a07032ba9f902be50
CFishman2002 <63076924+CFishman2002@users.noreply.github.com>
2020-06-19 14:08:34 -0400

    Update get_args.ps1

--- get_args.ps1
+++ get_args.ps1
@@ -3,3 +3,4 @@ $userobjects = Import-CSV Args.txt -Header idx,name,value
 $var_num = $userobjects[0].name
 Copy-Item Args.txt -Destination Args2.txt
 Write-Host "CLF $var_num"
+throw "Script HALTED!!"
